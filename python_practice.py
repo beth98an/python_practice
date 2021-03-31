f3754e6182df99c6b6f67321efe29520ce948104
beth98an <76478762+beth98an@users.noreply.github.com>
2021-03-31 10:53:38 +0100

    python practise file added

---
+++ python_practice.py
@@ -0,0 +1,13 @@
+# Python practice
+
+# Create a function called greetings that takes one argument as string and returns the name
+# def greetings(name):
+#     return name
+#
+# print(greetings("bob"))
+
+# # Declare a list of numbers from 1 to 9 iterate through the list and print the list
+# num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# for num in num_list:
+#     print(num)
+
